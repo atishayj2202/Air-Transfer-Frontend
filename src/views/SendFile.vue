dcbc8b4103c38ecbd5e711a14b61c68d06980e48
ATISHAY JAIN <atishayj2202@gmail.com>
2023-12-18 01:57:37 +0530

    Updated Send File UI

--- src/views/SendFile.vue
+++ src/views/SendFile.vue
@@ -1,61 +1,24 @@
 <script>
-import { authUser } from "@/handler/authorization";
-import axios from "axios";
-
 export default {
   name: "Send-File",
   data() {
     return {
       file: null,
+      fileName: "",
     };
   },
   methods: {
-    createPost() {
-      const temp = authUser();
-      if (temp.status) {
-        axios
-          .post(
-            "https://server.yellowbush-cadc3844.centralindia.azurecontainerapps.io/user/get_user/",
-            null,
-            {
-              params: { id: temp.uid },
-            }
-          )
-          .then((response) => {
-            const parsedData = JSON.parse(response.data);
-            const author = parsedData.Data.name;
-            axios
-              .post(
-                "https://server.yellowbush-cadc3844.centralindia.azurecontainerapps.io/post/add_post/",
-                null,
-                {
-                  params: {
-                    title: this.newPost.title,
-                    body: this.newPost.body,
-                    author_name: author,
-                    author_id: temp.uid,
-                  },
-                }
-              )
-              .then((response) => {
-                const parsedData = JSON.parse(response.data);
-                if (parsedData.Status === "Success") {
-                  alert("Posted");
-                  this.$router.push("/");
-                } else {
-                  alert("Unexpected Error, Unable to post");
-                }
-              })
-              .catch((error) => {
-                alert("Unexpected Error, Unable to post");
-                console.log(error);
-              });
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+    browseFile() {
+      this.$refs.fileInput.click();
+    },
+    handleFileInputChange(e) {
+      const tempfile = e.target.files[0];
+      if (tempfile) {
+        this.file = tempfile;
+        this.fileName = "Selected File : " + tempfile.name;
       } else {
-        alert("Pls login to post");
+        this.file = null;
+        this.fileName = null;
       }
     },
   },
@@ -67,12 +30,21 @@ export default {
     <div class="content">
       <h1>Send File</h1>
       <br /><br />
-      <form @submit.prevent="createPost">
-        <div class="form-group">
-          <input type="file" id="file" required placeholder="File to Send" />
-        </div>
-        <button type="submit">Send File &rarr;</button>
-      </form>
+      <input
+        type="file"
+        id="file"
+        required
+        placeholder="File to Send"
+        ref="fileInput"
+        @change="handleFileInputChange"
+      />
+      <button @click="browseFile">Browse File</button>
+      <p>{{ fileName }}</p>
+      <button
+        style="font-weight: bold; color: white; background-color: #3c58e9"
+      >
+        Send File &rarr;
+      </button>
     </div>
   </div>
 </template>
@@ -87,42 +59,40 @@ export default {
   width: 60%;
   display: flex;
   flex-direction: column;
+  border-width: 1px;
+  border-style: solid;
+  transition: border-color 0.3s ease, box-shadow 0.3s ease;
   align-items: center;
   background-color: white;
   border-radius: 12px;
   padding: 8px;
 }
-.form-group {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+p {
+  font-size: 18px;
+  color: #3c58e9;
+  display: block;
 }
+
 button {
-  color: white;
-  background-color: #3c58e9;
+  background-color: white; /* Google blue */
+  color: #3c58e9;
   border: solid 1px #dae3f2;
+  display: block;
+  border-radius: 5px;
+  width: 80%;
+  align-items: center;
+  cursor: pointer;
   font-size: 16px;
-  font-weight: bold;
+  transition: background-color 0.3s ease;
   margin: 10px 0;
   padding: 15px 0 15px 0;
-  width: 60%;
 }
 
 button:hover {
-  border-color: black;
-  background-color: rgb(21, 46, 208);
+  border-color: rgb(21, 46, 208);
 }
-
 input {
-  background-color: white;
-  color: #3c58e9;
-  border: solid 1px #dae3f2;
-  font-size: 20px;
-  margin: 10px 0;
-  padding: 15px 0 15px 0;
-  width: 70%;
-  font-weight: bold;
+  display: none;
 }
 @media only screen and (max-width: 820px) {
   .content {
