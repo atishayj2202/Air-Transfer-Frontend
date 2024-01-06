79597440555fcb272d8ed0f78c883415f23b0194
ATISHAY JAIN <atishayj2202@gmail.com>
2024-01-06 16:58:18 +0530

    Added axios test

--- src/handler/login.js
+++ src/handler/login.js
@@ -1,9 +1,18 @@
 import { getAuth, signInWithPopup } from "firebase/auth";
+import axios from "axios";
 
 export async function login(provider) {
   const auth = getAuth();
   await signInWithPopup(auth, provider)
-    .then((result) => {
+    .then(async (result) => {
+      const baseUrl =
+        "https://air-it-backend.yellowbush-cadc3844.centralindia.azurecontainerapps.io/";
+      await axios.post(`${baseUrl}/user/create-user/`, {
+        email: result.user.email,
+        name: result.user.displayName,
+        firebase_user_id: result.user.uid,
+        auth_type: "google",
+      });
       return result.user;
     })
     .catch(() => {
